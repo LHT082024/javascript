f25c6802c80e4928ca741c8bd47b6b4d606656e8
SapphireShy <lucielity@gmail.com>
2024-03-26 11:55:11 +0100

    done

--- script.js
+++ script.js
@@ -20,7 +20,60 @@ function greeting (){
 
 //lagringsplass = lagringsinnhold
 const isekaiButton = document.querySelector("#isekaiButton");
-console.log(isekaiButton);
+
+isekaiButton.addEventListener("click", greeting);
 
 
-isekaiButton.addEventListener("click", greeting)
+
+//world switchbutton.
+// HENT
+const WorldSwitchButton = document.querySelector("#WorldSwitchButton");
+
+// MODIFISER
+function worldSwitch(){
+    //HENT
+    const body = document.querySelector("body");
+
+    body.classList.toggle("cool");
+    
+}
+
+// SEND
+WorldSwitchButton.addEventListener("click", worldSwitch);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+isekaiButton.addEventListener("click", () => {
+    console.log("arrow function");
+
+
+    const firstName = "*splat*";
+    const greet = "Truck-kunnn";
+
+    console.log(`${greet}, ${firstName}!`)
+});
+*/
