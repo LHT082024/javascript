37d81bcb466c2cf6c20a12a13664bb8e5321e236
SapphireShy <lucielity@gmail.com>
2024-03-26 10:38:32 +0100

    stuff

--- README.md
+++
@@ -1,3 +0,0 @@
-# javascript
-
-[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/SapphireShy/javascript)
--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('Hello!');
+console.log  ('hello');
+
+
+
+//hilsen funksjonen
+function greeting (){
+     console.log ("dette er en greeting");
+
+     const firstName = "Lucia";
+     const greet = "Mornings";
+
+     console.log( greet + " " + firstName)
+     console.log(`${greet}, ${firstName}!`)
+}
+
+
+greeting();
+
+
+//DOMM (Document Object Model manipulation)
+
+//lagringsplass = lagringsinnhold
+const isekaiButton = document.querySelector("#isekaiButton");
+console.log(isekaiButton);
